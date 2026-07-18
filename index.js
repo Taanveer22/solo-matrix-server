59c7133877ccf73f19514d4c22670f25eaf1c959
Taanveer22 <taanveer469@gmail.com>
2026-07-18 22:24:26 +0600

    alhamdulliah read operation done for categorized jobs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // instance
@@ -8,6 +9,13 @@ const port = process.env.PORT || 5000;
 
 // middlewars
 app.use(express.json());
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.89rnkti.mongodb.net/?appName=Cluster0`;
 
@@ -24,6 +32,14 @@ async function run() {
     await client.connect();
     await client.db('admin').command({ ping: 1 });
     console.log('You successfully connected to MongoDB!');
+    // ==============================================================
+    const database = client.db('soloMatrixDB');
+    const jobsCollection = database.collection('jobs');
+    // =============== JOBS COLLECTION    ================
+    app.get('/jobs', async (req, res) => {
+      const result = await jobsCollection.find().toArray();
+      res.send(result);
+    });
   } catch (error) {
     console.error('Failed to connect to MongoDB', error);
   }
@@ -31,9 +47,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('server is running!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`app listening on port ${port}`);
 });
